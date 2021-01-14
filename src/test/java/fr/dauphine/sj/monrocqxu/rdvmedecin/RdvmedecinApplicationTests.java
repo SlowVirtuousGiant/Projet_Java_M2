f3ea29abc3f394f23aa2b4f1cf1c116e41dd2c75
SlowVirtuousGiant <nicolas.xu1997@gmail.com>
2021-01-14 18:52:57 +0100

    Starter

---
+++ src/test/java/fr/dauphine/sj/monrocqxu/rdvmedecin/RdvmedecinApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.dauphine.sj.monrocqxu.rdvmedecin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RdvmedecinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
