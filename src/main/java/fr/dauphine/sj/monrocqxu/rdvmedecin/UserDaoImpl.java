c8788d7d9d478ca233f744746c555fcb7504a48a
SlowVirtuousGiant <nicolas.xu1997@gmail.com>
2021-01-17 13:31:05 +0100

    connect DBMS

--- src/main/java/fr/dauphine/sj/monrocqxu/rdvmedecin/UserDaoImpl.java
+++ src/main/java/fr/dauphine/sj/monrocqxu/rdvmedecin/UserDaoImpl.java
@@ -2,7 +2,14 @@ package fr.dauphine.sj.monrocqxu.rdvmedecin;
 
 import java.util.List;
 
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceUnit;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+
 import org.hibernate.Criteria;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -10,12 +17,22 @@ import org.springframework.stereotype.Component;
 @Component
 public class UserDaoImpl implements PersonneDao {
 
-	@Autowired
+	@PersistenceUnit
+	private EntityManagerFactory entityManagerFactory;
+
+	/*@Autowired
 	private SessionFactory sessionFactory;
+	*/
+	
 	
 	public List getUserDetails() {
-		Criteria criteria = sessionFactory.openSession().createCriteria(Personne.class);
-		return criteria.list();
+		Session session=entityManagerFactory.unwrap(SessionFactory.class).openSession();
+		CriteriaBuilder builder = session.getCriteriaBuilder();
+        CriteriaQuery criteria = builder.createQuery(Personne.class);
+        Root contactRoot = criteria.from(Personne.class);
+        criteria.select(contactRoot);
+        return session.createQuery(criteria).getResultList();
+	
 	}
 
 }
