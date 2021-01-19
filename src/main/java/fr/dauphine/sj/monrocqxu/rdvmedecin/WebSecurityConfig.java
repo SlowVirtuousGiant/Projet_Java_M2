edab332d38f30f64c6cbb2411c43e66d458a5efa
SlowVirtuousGiant <nicolas.xu1997@gmail.com>
2021-01-19 01:06:51 +0100

    Auth specify Homepage

--- src/main/java/fr/dauphine/sj/monrocqxu/rdvmedecin/WebSecurityConfig.java
+++ src/main/java/fr/dauphine/sj/monrocqxu/rdvmedecin/WebSecurityConfig.java
@@ -39,7 +39,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-            .anyRequest().authenticated()
+        	.antMatchers("/accueil.html")
+        	.permitAll().anyRequest().authenticated()
             .and()
             .formLogin().permitAll()
             .usernameParameter("email")
