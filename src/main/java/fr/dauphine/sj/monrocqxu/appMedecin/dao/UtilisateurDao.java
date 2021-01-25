ab207b98869accb13b5ad4711da4296c28e2d22c
SlowVirtuousGiant <nicolas.xu1997@gmail.com>
2021-01-25 20:58:42 +0100

    small fix

--- src/main/java/fr/dauphine/sj/monrocqxu/appMedecin/dao/UtilisateurDao.java
+++ src/main/java/fr/dauphine/sj/monrocqxu/appMedecin/dao/UtilisateurDao.java
@@ -70,7 +70,6 @@ public class UtilisateurDao {
 	        session.beginTransaction();
 			session.save(utilisateur);
 			session.getTransaction().commit();
-			//HibernateUtil.getSessionFactory().
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
